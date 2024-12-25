627e36592dc7eee2fe237f558e3d2f067c455981
thangnq <thangnq@newit.co.jp>
2024-12-25 11:45:06 +0700

    fix UI login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,14 +28,14 @@
                 <input id="remember_me" type="checkbox"
                     class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
                     name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Ghi nhớ ') }}</span>
+                <span class="ms-2 text-sm text-white">{{ __('Ghi nhớ ') }}</span>
             </label>
-            <a class=" absolute right-0 ms-2 text-sm text-gray-600 dark:text-gray-400" href="/register">Bạn chưa có tài khoản?</a>
+            <a class=" absolute right-0 ms-2 text-sm text-white" href="/register">Bạn chưa có tài khoản?</a>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                <a class="underline text-sm text-white hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                     href="{{ route('password.request') }}">
                     {{ __('Bạn muốn lấy lại mật khẩu?') }}
                 </a>
